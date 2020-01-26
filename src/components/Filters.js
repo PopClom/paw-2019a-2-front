bcc264f837ac5429e8e5323ef4d0150aea50f558
Miguel <mdiluca@itba.edu.ar>
2020-01-26 05:10:48 -0300

    Mis primeros pasos en react

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { Trans } from 'react-i18next';
+import axios from "axios";
+import {SERVER_ADDR} from "../constants";
 
 class Filters extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tags: {},
+            tagsCheckboxes: {}
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`${SERVER_ADDR}/recipes/get_tags`).then(response => {
+            this.setState({tags: response.data.tags});
+            let tagsCheckboxes = {};
+            Object.keys(this.state.tags).map(idx => tagsCheckboxes[this.state.tags[idx]] = false);
+            this.setState({tagsCheckboxes});
+        });
+    }
+
+    onTagsChange(event) {
+        const val = event.target.checked;
+        const id = event.target.id;
+
+
+        let tagsCheckboxes = Object.assign({}, this.state.tagsCheckboxes, {[id]: val});
+        this.setState({tagsCheckboxes});
+    }
+
     render() {
+        const {tags} = this.state;
+
         return (
             <div id="filters-card">
                 <h4>
@@ -15,11 +45,26 @@ class Filters extends React.Component {
                     <input className="form-control" path="searchBar" placeholder="Search"/>
 
                     <label className="text-filter">
-                        <Trans>Sort by</Trans>
+                        <Trans>sortBy</Trans>
                     </label>
 
+                    <label className="text-filter">
+                        <Trans i18nKey="cuisineType"/>
+                    </label>
+                    <div className="filter-items">
+                        {Object.keys(tags).map(idx =>
+                            <div className="custom-control custom-checkbox">
+                                <input path="tags" value={tags[idx]} className="custom-control-input" id={tags[idx]}
+                                          name="groupTagFilter" type='checkbox' checked={this.state.tagsCheckboxes[tags[idx]]}
+                                          onChange={this.onTagsChange.bind(this)}/>
+                                <label className="custom-control-label" path="tags" htmlFor={tags[idx]}>
+                                    <Trans i18nKey={tags[idx]}/>
+                                </label>
+                            </div>)}
+                    </div>
+
                     <button className="btn btn-green btn-apply-filters" type="submit">
-                        <Trans>Confirm</Trans>
+                        <Trans>confirm</Trans>
                     </button>
                 </form>
             </div>
